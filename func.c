6a20f4bb3fa717ea2365d211bd4e1c565354eefd
Alan Vital <49616136+alanvitalp@users.noreply.github.com>
2021-03-09 15:22:48 -0300

    Update func.c

--- func.c
+++ func.c
@@ -3,7 +3,7 @@
 char sortear_letra () {
   char letras[] = "ABCDEFGHIJLMNOPQRSTUVXZ";
   
-  srand(time(NULL));
+  // srand(time(NULL));
   int random = rand() % (23);
 
   return letras[random];
@@ -20,4 +20,15 @@ char pedir_enter() {
 
 void limpar_tela() {
   system("clear"); // linux
+  system("cls"); // windows
+}
+
+char **criaVetorString(int n){
+  char **letras_usadas;
+    
+    letras_usadas = (char **) malloc(n * sizeof(char *));
+    for (int i =0; i < n ; i++) {
+       letras_usadas[i] = (char *) malloc(30*sizeof(char));
+    }
+    return letras_usadas;
 }
